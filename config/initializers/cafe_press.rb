79dddda61ccb384561cb2d77db6d99f5207e9068
Benjamin Smith <benjamin.lee.smith@gmail.com>
2010-11-23 16:24:41 -0500

    added cp config loading from ENV

--- config/initializers/cafe_press.rb
+++ config/initializers/cafe_press.rb
@@ -1,1 +1,9 @@
-::CAFEPRESS_CONFIG = YAML.load_file("#{::Rails.root.to_s}/config/cafepress.yml")[::Rails.env]
+begin
+  ::CAFEPRESS_CONFIG = YAML.load_file("#{::Rails.root.to_s}/config/cafepress.yml")[::Rails.env]
+rescue
+  ::CAFEPRESS_CONFIG = {
+    'app_key'  => ENV['cp_app_key'],
+    'email'    => ENV['cp_email'],
+    'password' => ENV['cp_password']
+  }
+end
