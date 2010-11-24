08d10f7b67e11d918d15b9fde14991f9d5e493d8
Benjamin Smith <benjamin.lee.smith@gmail.com>
2010-11-24 00:36:33 -0500

    setting root route

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ CafepressWrapper::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "stores#index"
 
   # See how all your routes lay out with "rake routes"
 
