9dfff3fd19bdc92b63237d73ccc950aebb16b07e
Benjamin Smith <benjamin.lee.smith@gmail.com>
2011-05-10 00:26:56 -0700

    added redirect when a product is loaded directly

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
   respond_to :js
   def show
-    respond_with(@product = Product.find(params[:id]))
+    respond_to do |format|
+      format.js {@product = Product.find(params[:id])}
+      format.html {
+        product = Product.find(params[:id])
+        redirect_to store_url(product.store, :product_id => product.id)
+      }
+    end
   end
 end
